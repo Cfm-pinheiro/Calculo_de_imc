f57e3f2fcc6dfce41e6d5c8e8e3eb0993052d2d0
Cfm_pinheiro <maxotus@google.comm>
2024-03-26 20:50:38 -0300

    Acrescentando a parte do resultado

--- imc.py
+++ imc.py
@@ -13,35 +13,23 @@ peso = float(input('Digite seu peso (kg): '))
 # Calculo do (IMC)
 imc = calcular_IMC(peso, altura)
 
-print(f'Seu IMC é de: {imc:.2f}')
+classificacao =(f'Seu IMC é de: {imc:.2f}')
 
 
 # Logica do programa
 if imc < 18.5:
-    print('Você está abaixo do peso ideal.')
+    classificacao = 'Abaixo do peso'
 elif 18.5 <= imc < 24.9:
-    print('Você está no peso ideal.')
+    classificacao = 'Peso normal'
 elif 24.9 <= imc < 29.9:
-    print('Você está acima do peso ideal (sobrepeso).')
+    classificacao = 'Sobrepesso'
 elif 29.9 <= imc < 34.9:
-    print('Obesidade grau I.')
+    classificacao = ('Obesidade grau I.')
 elif 34.9 <= imc < 39.9:
-    print('Obesidade grau II (severa).')
+    classificacao = ('Obesidade grau II (severa).')
 else:
-    print('Obesidade grau III (mórbida).')
+    classificacao = ('Obesidade grau III (mórbida).')
 
-    
-    linha_3 = f'{imc:.2f}'
-    print(linha_3)
-
-'''
-linha_1 = f'{nome} tem {altura:.2f} de altura'
-linha_2 = f'pesa {peso} quilos e seu imc é'
-linha_3 = f'{imc:.2f}'
-
-
-print(linha_1)
-print(linha_2)
-print(linha_3)
-
-'''
+# Resultados
+print(f'seu IMC é de : {imc:.2f}')
+print(f'Sua classificação é de : {classificacao}')
